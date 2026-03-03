2ad98741d81b21e9388fc78d87eb4c29f076c01b
Dasari Muralidhar <153696275+muralidhar24@users.noreply.github.com>
2026-03-03 15:40:00 +0530

    first commit

---
+++ exp3/UNIXCMDs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+int main(int argc, char **argv)
+{
+char buffer[1024];
+int files[2];
+ssize_t count;
+/* Check for insufficient parameters */
+if (argc < 3)
+return -1;
+files[0] = open(argv[1], O_RDONLY);
+if (files[0] == -1)
+return -1;
+files[1] = open(argv[2], O_WRONLY | O_CREAT | O_TRUNIC , S_IRUSR | S_IWUSR);
+if (files[1] == -1)
+{
+close(files[0]);
+return -1;
+}
+while ((count = read(files[0], buffer, sizeof(buffer))) != 0)
+write(files[1], buffer, count);
+return 0;
+}
+
