171e1af7087544049ef5cabd6e0477e9f4776976
Dasari Muralidhar <153696275+muralidhar24@users.noreply.github.com>
2026-03-05 22:10:27 +0530

    first commit

---
+++ exp7/producerconsumer.c
@@ -0,0 +1,62 @@
+  #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#define BUFFER_SIZE 5         // Size of the shared buffer
+
+int buffer[BUFFER_SIZE]; 
+int in = 0; 
+int out = 0; 
+sem_t empty; 
+sem_t full;  
+pthread_mutex_t mutex;
+void *producer(void *arg) 
+{
+	int i;
+    for (i = 1; i <= 10; i++) 
+	{ 
+        sem_wait(&empty); 
+        pthread_mutex_lock(&mutex); 
+        buffer[in] = i; 
+        printf("Producer produced: %d\n", i);
+        in = (in + 1) % BUFFER_SIZE; 
+        pthread_mutex_unlock(&mutex); 
+        sem_post(&full); 
+        sleep(1); 
+    }
+    return NULL;
+}
+void *consumer(void *arg)
+ {
+ 	int i;
+    for (i = 1; i <= 10; i++) 
+{ 
+        sem_wait(&full); 
+        pthread_mutex_lock(&mutex); 
+
+        int item = buffer[out]; 
+        printf("Consumer consumed: %d\n", item);
+        out = (out + 1) % BUFFER_SIZE; 
+        pthread_mutex_unlock(&mutex); 
+        sem_post(&empty); 
+        sleep(2); 
+    }
+    return NULL;
+}
+
+int main()
+ {
+    pthread_t prodThread, consThread;
+    sem_init(&empty, 0, BUFFER_SIZE);
+    sem_init(&full, 0, 0); 
+    pthread_mutex_init(&mutex, NULL);
+    pthread_create(&prodThread, NULL, producer, NULL);
+    pthread_create(&consThread, NULL, consumer, NULL);
+    pthread_join(prodThread, NULL);
+    pthread_join(consThread, NULL);
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    pthread_mutex_destroy(&mutex);
+   return 0;
+}
+
